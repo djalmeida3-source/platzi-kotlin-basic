0c61ad865be8fadffaa229cdf43052f9ac117929
David Almeida <david.almeida@ninjaone.com>
2024-03-18 14:44:05 -0500

    Adding when example

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -2,16 +2,27 @@ package org.example
 
 const val PI= 3.1416
 fun main() {
-    val nombre = "Li"
+    val nombreColor = "Carmesi"
 
-    if (nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}") else println("La variable nombre esta vacia")
+    when (nombreColor) {
+        "Amarillo" -> println("El color es amarillo")
+        "Rojo","Carmesi" -> println("Este color simboliza el calor")
+        else -> println("Error: No tengo informacion del color")
+    }
 
-    val mensaje : String = if (nombre.length > 4) {
-        "Tu nombre es largo"
-    } else if (nombre.isEmpty()) {
-        "Tu nombre esta vacio"
-    } else {
-        "Tienes un nombre corto"
+    val code = 404
+    when (code) {
+        in 200..299 -> println("Todo ha ido bien")
+        in 400 .. 500 -> println("Algo ha fallado")
+        else -> println("Codigo desconocido, algo ha fallado")
+    }
+
+    val tallDeZapatos = 46
+    val mensaje = when (tallDeZapatos) {
+        41,43 -> "Tenemos disponibles"
+        42,44 -> "Casi no nos quedan"
+        45 -> "Estamos agotados"
+        else -> "Estos zapatos solo vienen en tallas 41,42,43,44 y 45"
     }
     println(mensaje)
 }
