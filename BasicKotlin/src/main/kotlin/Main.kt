cf6e62566cd9fa88f9fd7c4bb7814381e31603fa
David Almeida <david.almeida@ninjaone.com>
2024-03-18 14:30:44 -0500

    Adding if example

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -2,30 +2,16 @@ package org.example
 
 const val PI= 3.1416
 fun main() {
-    println("Hello World!")
-    var dinero = 10
-    println(dinero)
-    dinero = 5
-    println(dinero)
+    val nombre = "Li"
 
-    val nombre = "Maria"
-    println(nombre)
-    println(PI)
+    if (nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}") else println("La variable nombre esta vacia")
 
-    val boolean = true
-    val numeroLargo = 3L
-    val duble = 2.7182
-    val float = 1.1f
-
-    val primerValor = 20
-    val segundoValor = 10
-    val tercerValor = primerValor - segundoValor
-    println(tercerValor)
-
-    val apellido = "Almeida"
-    val david = "David"
-    var nombreCompleto = david + apellido
-    println(nombreCompleto)
-    nombreCompleto = "Mi nombre es: $david $apellido"
-    println(nombreCompleto)
+    val mensaje : String = if (nombre.length > 4) {
+        "Tu nombre es largo"
+    } else if (nombre.isEmpty()) {
+        "Tu nombre esta vacio"
+    } else {
+        "Tienes un nombre corto"
+    }
+    println(mensaje)
 }
