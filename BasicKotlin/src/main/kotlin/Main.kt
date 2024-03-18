1e6460bc2400e7c92e65c2bd91b1a7edbd4e3678
David Almeida <david.almeida@ninjaone.com>
2024-03-18 10:11:39 -0500

    Adding different types of variables

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -11,4 +11,21 @@ fun main() {
     val nombre = "Maria"
     println(nombre)
     println(PI)
+
+    val boolean = true
+    val numeroLargo = 3L
+    val duble = 2.7182
+    val float = 1.1f
+
+    val primerValor = 20
+    val segundoValor = 10
+    val tercerValor = primerValor - segundoValor
+    println(tercerValor)
+
+    val apellido = "Almeida"
+    val david = "David"
+    var nombreCompleto = david + apellido
+    println(nombreCompleto)
+    nombreCompleto = "Mi nombre es: $david $apellido"
+    println(nombreCompleto)
 }
