f04753256b8713e4ca88fdeec3c19c138a74f305
David Almeida <david.almeida@ninjaone.com>
2024-03-19 11:51:02 -0500

    Adding cicles example

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -2,15 +2,15 @@ package org.example
 
 const val PI= 3.1416
 fun main() {
-    var contador = 10
-    while (contador > 0) {
-        println("El valor del contador es $contador")
-        contador--
-    }
+    val listaDeFrutas: List<String> = listOf("Manzana","Pera","Frambuesa","Durazno")
+    for (fruta in listaDeFrutas) println("Hoy voy a comer una fruta llamada $fruta")
 
-    do {
-        println("Generando numeros aleatorios...")
-        val numeroAleatorio = (0..100).random()
-        println("El numero generado es $numeroAleatorio")
-    } while (numeroAleatorio > 50)
+    listaDeFrutas.forEach { fruta -> println("Hoy voy a comer una fruta nueva llamada $fruta") }
+
+    val caracteresDeFruta: List<Int> = listaDeFrutas.map { fruta -> fruta.length }
+    println(caracteresDeFruta)
+
+    val listaFiltrada = caracteresDeFruta.filter { caracteres -> caracteres > 5 }
+    println(listaFiltrada)
+
 }
