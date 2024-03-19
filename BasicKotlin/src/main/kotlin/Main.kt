70bc57234cba8fc408a29907caad97624f2fb90a
David Almeida <david.almeida@ninjaone.com>
2024-03-19 13:42:18 -0500

    Adding elvis operator example

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -1,17 +1,7 @@
 package org.example
 
 fun main() {
-    var nombre :String? = null
-    try {
-        throw NullPointerException("Referencia nula")
-    } catch (e:NullPointerException){
-        println("Ha ocurrido un error")
-    } finally {
-        println("Finalmente ha ocurrido un error... Cerrando aplicacion")
-    }
-
-    val primerValor = 10
-    val segundoValor = 0
-    val resultado : Int = try { primerValor / segundoValor } catch (e: Exception) { 0 }
-    println(resultado)
+    var nombre: String? = null
+    val caracteresDeNombre : Int = nombre?.length ?: 0
+    println(caracteresDeNombre)
 }
