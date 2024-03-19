41a8507f5230053667e6042e2b8126d20001b4ad
David Almeida <david.almeida@ninjaone.com>
2024-03-19 13:55:07 -0500

    Adding lists example

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
 package org.example
 
 fun main() {
-    var nombre: String? = null
-    val caracteresDeNombre : Int = nombre?.length ?: 0
-    println(caracteresDeNombre)
+    val listaDeNombres = listOf("Juan", "Enrique", "Camila")
+    println(listaDeNombres)
+
+    val listaVacia = mutableListOf<String>()
+    println(listaVacia)
+    listaVacia.add("Juan")
+    println(listaVacia)
+
+    val valorUsandoGet = listaVacia.get(0)
+    println(valorUsandoGet)
+
+    val valorUsandoOperador = listaVacia[0]
+    println(valorUsandoOperador)
+
+    val primerValor: String? = listaDeNombres.firstOrNull()
+    println(primerValor)
+
+    listaVacia.removeAt(0)
+    println(listaVacia)
+
+    listaVacia.add("Enrique")
+    println(listaVacia)
+    listaVacia.removeIf{ caracteres -> caracteres.length > 3 }
+    println(listaVacia)
+
+    val myArray = arrayOf(1, 2, 3, 4)
+    println(myArray)
+    println("Array como lista ${myArray.toList()}")
 }
