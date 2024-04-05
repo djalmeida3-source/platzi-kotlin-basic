13628c4ad58294b3d682ab2250eeba53df0f11e7
David Almeida <david.almeida@ninjaone.com>
2024-04-05 14:24:15 -0500

    Example of Also

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -2,14 +2,8 @@ package org.example
 
 fun main() {
     val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Huawei P30", "Xiaomi Mi 10")
-        .apply {
-            removeIf { movil -> movil.contains("Google") }
-        }
+        .also {
+            lista -> println("Nuestros moviles son: $lista")
+        }.asReversed()
     println(moviles)
-
-    val colores : MutableList<String>? = mutableListOf("Rojo", "Verde", "Azul")
-    colores?.apply {
-        println("Nuestros colores son: $this")
-        println("El tamaño de la lista es: $size")
-    }
 }
