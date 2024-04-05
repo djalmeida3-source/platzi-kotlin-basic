13da668f9c10a7584051e99a8aa4a9ec6960e439
David Almeida <david.almeida@ninjaone.com>
2024-04-05 14:21:02 -0500

    Example of Apply

--- BasicKotlin/src/main/kotlin/Main.kt
+++ BasicKotlin/src/main/kotlin/Main.kt
@@ -2,9 +2,14 @@ package org.example
 
 fun main() {
     val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Huawei P30", "Xiaomi Mi 10")
-        .run {
+        .apply {
             removeIf { movil -> movil.contains("Google") }
-            this
         }
     println(moviles)
+
+    val colores : MutableList<String>? = mutableListOf("Rojo", "Verde", "Azul")
+    colores?.apply {
+        println("Nuestros colores son: $this")
+        println("El tamaño de la lista es: $size")
+    }
 }
